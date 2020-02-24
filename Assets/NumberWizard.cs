6c14163980e64e84a650c96bf1eef995958728af
Grzegorz Kazimierczak <grzegorz.kazimierczak.sg@gmail.com>
2020-02-24 22:50:33 +0100

    U2Final -  refactor code, add methods StartGame and NextGuess

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -4,18 +4,27 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour
 {
-    int min = 1;
-    int max = 1000;
-    int guess = 500;
+    int min;
+    int max;
+    int guess;
     // Start is called before the first frame update
     void Start()
     {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        min = 1;
+        max = 1000;
+        guess = 500;
+
         Debug.Log("Witaj w Grze Number Wizard!");
         Debug.Log("Wybierz liczbę z zakresu");
         Debug.Log("Najmniejsza to: " + min);
         Debug.Log("Największa to: " + max);
         Debug.Log("Powiedz mi czy twoja liczba jest mniejsza czy większa od " + guess);
-        Debug.Log("Do góry = Większa, Do Dołu = Mniejsza, Enter = Trafiłem");
+        Debug.Log("Do góry = Większa, Do Dołu = Mniejsza, Enter = Trafiłem");        
     }
 
     // Update is called once per frame
@@ -23,26 +32,30 @@ public class NumberWizard : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            Debug.Log("Wcisnąłeś Do góry!");
+            //Debug.Log("Wcisnąłeś Do góry!");
             min = guess + 1;
-            guess = (min + max) / 2;
-            Debug.Log("Powiedz mi czy twoja liczba jest mniejsza czy większa od " + guess);
+            NextGuess();
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            Debug.Log("Wcisnąłeś Do dołu!");
+            //Debug.Log("Wcisnąłeś Do dołu!");
             if (guess > 1)
                 max = guess - 1;
             else
                 max = guess;
-            guess = (min + max) / 2;
-            Debug.Log("Powiedz mi czy twoja liczba jest mniejsza czy większa od " + guess);
+            NextGuess();
         }
         else if (Input.GetKeyDown(KeyCode.Return))
         {
-            Debug.Log("Wcisnąłeś Enter.");
-            Debug.Log(guess);
+            //Debug.Log("Wcisnąłeś Enter.");
+            Debug.Log("To liczba" + guess);
+            StartGame();
         }
+    }
 
+    void NextGuess()
+    {
+        guess = (min + max) / 2;
+        Debug.Log("Powiedz mi czy twoja liczba jest mniejsza czy większa od " + guess);
     }
 }
