5baf8602690cc44419173f4b7540daab8d6dd00b
Grzegorz Kazimierczak <grzegorz.kazimierczak.sg@gmail.com>
2020-02-24 20:04:26 +0100

    U2P4 - add inputs

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -7,20 +7,31 @@ public class NumberWizard : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int min=1;
-        int max=1000;
+        int min = 1;
+        int max = 1000;
 
-        
-        
         Debug.Log("Witaj w Grze Number Wizard!");
         Debug.Log("Wybierz liczbę z zakresu");
-        Debug.Log("Najmniejsza to: 1");
-        Debug.Log("Największa to: 1000");
+        Debug.Log("Najmniejsza to: " + min);
+        Debug.Log("Największa to: " + max);
+        Debug.Log("Powiedz mi czy twija liczba jest mniejsza czy większa od 500");
+        Debug.Log("Do góry = Większa, Do Dołu = Mniejsza, Enter = Trafiłem");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("Wcisnąłeś Do góry!");
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("Wcisnąłeś Do dołu!");
+        }
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Wcisnąłeś Enter.");
+        }
     }
 }
