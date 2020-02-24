8a0e08f8c390d280a4310ece43bef32a7adb786b
Grzegorz Kazimierczak <grzegorz.kazimierczak.sg@gmail.com>
2020-02-24 19:32:08 +0100

    U1P1 - create WizardNumber class

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Witaj w Grze Number Wizard!");
+        Debug.Log("Wybierz liczbę z zakresu");
+        Debug.Log("Najmniejsza to: 1");
+        Debug.Log("Największa to: 1000");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
